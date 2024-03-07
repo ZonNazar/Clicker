2687dea21ad9648b7600b7de5bf88194547ca698
Nazar <zongithub@gmail.com>
2024-03-07 19:16:25 +0200

    Implement move and copy constructors

--- Upgrade.cpp
+++ Upgrade.cpp
@@ -1,5 +1,28 @@
-//
-// Created by Babay on 07.03.2024.
-//
-
 #include "Upgrade.h"
+
+std::vector<Upgrade> Upgrade::bought_upgrades = {};
+
+
+Upgrade Upgrade::find_by_name(const std::string &name){}
+
+
+Upgrade::Upgrade()
+: Upgrade{"Noname", "empty"} {}
+
+Upgrade::Upgrade(const std::string &name)
+:Upgrade{name, "empty"} {}
+
+Upgrade::Upgrade(const std::string &name, const std::string &description)
+:_name{name}, _description{description}{
+    bought_upgrades.emplace_back(*this);
+}
+
+Upgrade::Upgrade(Upgrade &&other) noexcept
+: _name{other._name}, _description{other._description}{
+    bought_upgrades.emplace_back(*this);
+}
+
+Upgrade::Upgrade(const Upgrade &other)
+:_name{other._name}, _description{other._description}{
+    bought_upgrades.emplace_back(*this);
+}
--- Upgrade.h
+++ Upgrade.h
@@ -1,14 +1,24 @@
-//
-// Created by Babay on 07.03.2024.
-//
-
 #ifndef CLICKER_UPGRADE_H
 #define CLICKER_UPGRADE_H
-
+#include <iostream>
+#include <vector>
 
 class Upgrade {
+public:
+    static std::vector<Upgrade> bought_upgrades;
+private:
+    std::string _name;
+    std::string _description;
+public:
+    static Upgrade find_by_name(const std::string &name);
 
+    Upgrade();
+    Upgrade(const std::string &name);
+    Upgrade(const std::string &name, const std::string &description);
+    Upgrade(const Upgrade &other);
+    Upgrade(Upgrade &&other) noexcept;
+    ~Upgrade();
 };
 
 
-#endif //CLICKER_UPGRADE_H
+#endif

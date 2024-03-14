380d508400279a39d913bf0e26f08e7da4f6420e
Nazar <zongithub@gmail.com>
2024-03-14 18:24:47 +0200

    Add uploading and downloading info about player static functions

--- Player.cpp
+++ Player.cpp
@@ -5,6 +5,18 @@ int Player::RandomNumber(int a, int b) {
     std::uniform_int_distribution<int> distribution(a,b);
     return distribution(rd);
 }
+void Player::set_new_name(const std::string &new_name) {
+    _name = new_name;
+}
+void Player::set_new_email(const std::string &new_email) {
+    _email = new_email;
+}
+void Player::set_new_id(int new_id) {
+    _id = new_id;
+}
+void Player::set_new_score(int new_score) {
+    _killed_enemies = new_score;
+}
 void Player::show_info() const {
     std::cout << "Current score:" << _killed_enemies << std::endl;
 }
@@ -18,6 +30,29 @@ std::ostream &operator<<(std::ostream &os, const Player &player){
     os << "\n This player have killed: " << player._killed_enemies << "enemies";
     return os;
 }
+
+void Player::upload_info_about_player(const Player &player, std::ofstream &ofs, const std::string &path) {
+    ofs.open(path+"/"+player._name+".txt", std::ofstream::out);
+    ofs << player._name << std::endl
+    << player._email << std::endl
+    << player._id << std::endl
+    << player._killed_enemies;
+    ofs.close();
+}
+
+void Player::download_info_about_player(Player &player, std::ifstream &ifs, const std::string &path) {
+    ifs.open(path,std::iostream::out);
+    std::string currentLine;
+    getline(ifs,currentLine);
+    player.set_new_name(currentLine);
+    getline(ifs,currentLine);
+    player.set_new_email(currentLine);
+    getline(ifs,currentLine);
+    player.set_new_id(std::stoi(currentLine));
+    getline(ifs,currentLine);
+    player.set_new_score(std::stoi(currentLine));
+    ifs.close();
+}
 Player::Player()
 : Player{"Frank", "emptyemail@gmail.com", RandomNumber(0,1000)}{}
 
@@ -28,6 +63,6 @@ Player::Player(const std::string &name, const std::string &email)
 : Player{name, email, RandomNumber(0,1000)}{}
 
 Player::Player(const std::string &name, const std::string &email, int id)
-:_name{name},_email{email}, _id{id}{}
+:_name{name},_email{email}, _id{id}, _killed_enemies{0}{}
 
 Player::~Player(){}
--- Player.h
+++ Player.h
@@ -1,6 +1,7 @@
 #ifndef CLICKER_PLAYER_H
 #define CLICKER_PLAYER_H
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include "Upgrade.h"
 #include "Weapon.h"
@@ -14,16 +15,22 @@ private:
     Weapon currentWeapon;
     std::vector<Upgrade> purchased_upgrades;
 public:
-    virtual void show_info() const override;
-    int RandomNumber(int a, int b);
     Player();
     Player(const std::string &name);
     Player(const std::string &name, const std::string &email);
     Player(const std::string &name, const std::string &email, int id);
+    ~Player();
+    void set_new_name(const std::string &new_name);
+    void set_new_email(const std::string &new_email);
+    void set_new_id(int new_id);
+    void set_new_score(int new_score);
     void operator+(const Player &player);
     void operator++();
+    virtual void show_info() const override;
+    static void upload_info_about_player(const Player &player, std::ofstream &ofs, const std::string &path);
+    static void download_info_about_player(Player &player, std::ifstream &ifs, const std::string &path);
+    int RandomNumber(int a, int b);
     friend std::ostream &operator<<(std::ostream &os, const Player &player);
-    ~Player();
 };
 
 

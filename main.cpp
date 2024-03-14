9fc969f1efd1cffaf091df63e2b3f906686a0717
Nazar <zongithub@gmail.com>
2024-03-14 18:25:29 +0200

    Start login stage

--- main.cpp
+++ main.cpp
@@ -1,23 +1,56 @@
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 #include "Player.h"
 #include "Upgrade.h"
 #include "Enemy.h"
 #include "Mob.h"
 #include "Weapon.h"
 
+namespace fs = std::filesystem;
+
 int main() {
+    system("clear");
     std::string command;
     Player player;
-    Enemy *enemy = new Mob("noth");
-
-    delete enemy;
-
-    int numOfClicks = 0;
+    try{
+        fs::create_directory("Users");
+    }catch(...){}
+    std::cout << "Hi! Can I know your name before we get started?"<<std::endl;
+    getline(std::cin, command);
+    if(command == "admin")
+    {
+        std::cout << "Please, enter the password:";
+    }else{
+        bool found_user = false;
+        for(const auto &file : fs::directory_iterator("Users")){
+            if(command+".txt" == file.path().filename().string()) {
+                std::ifstream ifs;
+                Player::download_info_about_player(player,ifs, file.path().string());
+                std::cout << "Hi, " << command << "! It`s nice to see you again!"
+                          << "Press Enter to start" << std::endl;
+                getline(std::cin, command);
+                found_user = true;
+                break;
+            }
+        }
+        if(!found_user) {
+            std::cout << "Sorry but I don`t know you yet. Wanna create account?" << std::endl;
+            getline(std::cin, command);
+            if(command == "yes"){
+                std::cout << "Sorry, what`s your name again?" << std::endl;
+                getline(std::cin, command);
+                Player temp_player(command);
+                std::ofstream file_to_write;
+                Player::upload_info_about_player(temp_player,file_to_write, "Users");
+                std::cout << "Okay, " << command << ", let the journey begin!" << std::endl;
+            } else return 0;
+        }
+    }
     while(command != "exit"){
         getline(std::cin, command);
-        if(command.empty()) numOfClicks++;
+        if(command.empty())
         system("clear");
-        std::cout << "Number of clicks: "<< numOfClicks << std::endl;
     }
     return 0;
 }

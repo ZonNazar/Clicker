24597989e2e8009f1fa4d9337c3df4f45da34850
Nazar <zongithub@gmail.com>
2024-03-15 10:34:46 +0200

    Add admin/player login system

--- main.cpp
+++ main.cpp
@@ -8,8 +8,26 @@
 #include "Weapon.h"
 
 namespace fs = std::filesystem;
-
+void admin_login(std::string &command)
+{
+    while(command != "exit"){
+        getline(std::cin, command);
+        if(command.empty())
+            system("clear");
+    }
+}
+void player_login(std::string &command, Player &player)
+{
+    while(command != "exit"){
+        getline(std::cin, command);
+        if(command.empty()) player++;
+            system("clear");
+    }
+}
 int main() {
+    std::ofstream password("password", std::ofstream::out);
+    password << "8246";
+    password.close();
     system("clear");
     std::string command;
     Player player;
@@ -21,8 +39,31 @@ int main() {
     if(command == "admin")
     {
         std::cout << "Please, enter the password:";
+        getline(std::cin, command);
+        std::ifstream check_password("password", std::ifstream::out);
+        std::string temp_password;
+        getline(check_password, temp_password);
+        if(command == temp_password){
+            admin_login(command);
+            return 0;
+        }
+        else{
+            int number_of_tries = 2;
+            while (number_of_tries != 0)
+            {
+                std::cout << "Please, try again" << std::endl;
+                getline(std::cin, command);
+                number_of_tries--;
+                system("clear");
+                if(command == temp_password){
+                    admin_login(command);
+                    return 0;
+                }
+            }
+            std::cout << "You are not him. Press enter to exit and don`t bother me again";
+            return 0;
+        }
     }else{
-        bool found_user = false;
         for(const auto &file : fs::directory_iterator("Users")){
             if(command+".txt" == file.path().filename().string()) {
                 std::ifstream ifs;
@@ -30,27 +71,21 @@ int main() {
                 std::cout << "Hi, " << command << "! It`s nice to see you again!"
                           << "Press Enter to start" << std::endl;
                 getline(std::cin, command);
-                found_user = true;
-                break;
+                player_login(command, player);
+                return 0;
             }
         }
-        if(!found_user) {
-            std::cout << "Sorry but I don`t know you yet. Wanna create account?" << std::endl;
-            getline(std::cin, command);
-            if(command == "yes"){
-                std::cout << "Sorry, what`s your name again?" << std::endl;
-                getline(std::cin, command);
-                Player temp_player(command);
-                std::ofstream file_to_write;
-                Player::upload_info_about_player(temp_player,file_to_write, "Users");
-                std::cout << "Okay, " << command << ", let the journey begin!" << std::endl;
-            } else return 0;
-        }
-    }
-    while(command != "exit"){
+        std::cout << "Sorry but I don`t know you yet. Wanna create account?" << std::endl;
         getline(std::cin, command);
-        if(command.empty())
-        system("clear");
+        if(command == "yes") {
+            std::cout << "Sorry, what`s your name again?" << std::endl;
+            getline(std::cin, command);
+            Player temp_player(command);
+            std::ofstream file_to_write;
+            Player::upload_info_about_player(temp_player, file_to_write, "Users");
+            std::cout << "Okay, " << command << ", let the journey begin!" << std::endl;
+            player_login(command, temp_player);
+            return 0;
+        }else return 0;
     }
-    return 0;
 }

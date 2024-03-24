2f00a7216cef420ef67e2bd59971d5462e9485f8
Nazar <zongithub@gmail.com>
2024-03-24 12:28:53 +0200

    Add Weapon class and implement deafault constructors/destructor

--- Weapon.cpp
+++ Weapon.cpp
@@ -1,1 +1,13 @@
 #include "Weapon.h"
+
+Weapon::Weapon()
+        : Weapon{"Noname", 1, 1} {}
+
+Weapon::Weapon(const std::string &name)
+        : Weapon{name, 1, 1} {}
+
+Weapon::Weapon(const std::string &name, int level)
+        : Weapon{name, level, 1} {}
+
+Weapon::Weapon(const std::string &name, int level, int damage)
+        : _name{name}, _level{level}, _damage{damage} {}
--- Weapon.h
+++ Weapon.h
@@ -1,9 +1,23 @@
 #ifndef CLICKER_WEAPON_H
 #define CLICKER_WEAPON_H
 
+#include <iostream>
 
 class Weapon {
+private:
+    std::string _name;
+    int _level;
+    int _damage;
+public:
+    Weapon();
 
+    Weapon(const std::string &name);
+
+    Weapon(const std::string &name, int level);
+
+    Weapon(const std::string &name, int level, int damage);
+
+    ~Weapon();
 };
 
 
